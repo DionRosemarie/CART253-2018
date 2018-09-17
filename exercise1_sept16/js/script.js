494a67418ced9b3fa089cea0d33d9d428e4ef6a9
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2018-09-16 23:43:13 -0400

    New texture
    I changed the first background texture for a new one made in Illustrator

--- exercise1_sept16/js/script.js
+++ exercise1_sept16/js/script.js
@@ -8,30 +8,22 @@
 
 
 // The image of a clown face
-
-var shrekImage;
-
+var wandImage;
 // The current position of the clown face
-var shrekImageX;
-var shrekImageY;
+var wandImageX;
+var wandImageY;
+
+// The image of a unicorn
+var unicornImage;
+// The current position of the unicorn
+var unicornImageX;
+var unicornImageY;
 
 // The transparent image of "felt" that wipes down the canvas
-var swampTextureImage;
-
+var rainbowTexture;
 // The current position of the transparent image of "felt"
-var swampTextureImageX;
-var swampTextureImageY;
-
-// The image of a the donkey
-
-var donkeyImage;
-
-// The current position of the clown face
-var donkeyImageX;
-var donkeyImageY;
-
-
-
+var rainbowTextureX;
+var rainbowTextureY;
 
 
 // preload()
@@ -39,9 +31,9 @@ var donkeyImageY;
 // Load the two images we're using before the program starts
 
 function preload() {
-  shrekImage = loadImage("assets/images/shrek.png");
-  swampTextureImage = loadImage("assets/images/swamp.jpg");
-  donkeyImage = loadImage("assets/images/donkey.jpg");
+  wandImage = loadImage("assets/images/wand.png");
+  unicornImage = loadImage("assets/images/unicorn.png");
+  rainbowTexture = loadImage("assets/images/rainbow.png");
 }
 
 
@@ -51,19 +43,19 @@ function preload() {
 
 function setup() {
   // Create our canvas
-  createCanvas(640,640);
+  createCanvas(500,500);
 
   // Start the clown image at the centre of the canvas
-  shrekImageX = width/2;
-  shrekImageY = height/2;
+  wandImageX = width/2;
+  wandImageY = height/2;
 
-  // Start of the donkey from left to right
-  donkeyImageX = width/2;
-  donkeyImageY = 0 - donkeyImage.height/2;
+  // Start the unicorn movement from the left of the canvas
+  unicornImageX = 0 - unicornImage.height/2;
+  unicornImageY = width/2; 
 
   // Start the felt image perfectly off screen above the canvas
-  swampTextureImageX = width/2;
-  swampTextureImageY = 0 - swampTextureImage.height/2;
+  rainbowTextureX = width/2;
+  rainbowTextureY = 0 - rainbowTexture.height/2;
 
   // We'll use imageMode CENTER for this script
   imageMode(CENTER);
@@ -78,21 +70,26 @@ function setup() {
 function draw() {
 
   // Move the felt image down by increasing its y position
-  swampTextureImageY += 1;
-
+  rainbowTextureY += 1;
 
   // Display the felt image
-  image(swampTextureImage,swampTextureImageX,swampTextureImageY);
+  image(rainbowTexture,rainbowTextureX,rainbowTextureY);
+
+  // Move unicorn from it last position
+  unicornImageX += 1;
+
+  // Display the unicorn image
+  image(unicornImage,unicornImageX,unicornImageY);
 
   // Move the clown by moving it 1/10th of its current distance from the mouse
 
   // Calculate the distance in X and in Y
-  var xDistance = mouseX - shrekImageX;
-  var yDistance = mouseY - shrekImageY;
+  var xDistance = mouseX - wandImageX;
+  var yDistance = mouseY - wandImageY;
   // Add 1/10th of the x and y distance to the clown's current (x,y) location
-  shrekImageX = shrekImageX + xDistance/10;
-  shrekImageY = shrekImageY + yDistance/10;
+  wandImageX = wandImageX + xDistance/10;
+  wandImageY = wandImageY + yDistance/10;
 
   // Display the clown image
-  image(shrekImage,shrekImageX,shrekImageY);
+  image(wandImage,wandImageX,wandImageY);
 }
