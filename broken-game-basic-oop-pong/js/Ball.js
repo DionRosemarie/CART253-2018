2a85f4ef78e0f32b581ddb588243ebb4522932a4
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2018-10-30 16:49:17 -0400

    Ex6 : ball.js 31
    There was a + missing for the velocity

--- broken-game-basic-oop-pong/js/Ball.js
+++ broken-game-basic-oop-pong/js/Ball.js
@@ -27,7 +27,8 @@ function Ball(x,y,vx,vy,size,speed) {
 Ball.prototype.updated = function () {
 //////////////////////// FIXED /////////////////////////////////
   // Update position with velocity
-  this.x = this.vx;
+  // it is suppose to be += and not only =
+  this.x += this.vx;
   this.y += this.vy;
 
   // Constrain y position to be on screen
