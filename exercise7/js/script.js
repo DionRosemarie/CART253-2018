e9d1b7d94aa538d5c17205cc78651bac5557d5d0
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2018-11-13 13:52:49 -0500

    Ex7 : Describing the main idea of the project 3 game

--- exercise7/js/script.js
+++ exercise7/js/script.js
@@ -1,10 +1,13 @@
 /*****************
 
-Title of Project
-Author Name
+Outer Space - Project 3 prototype
+Rose-Marie Dion
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+For this prototype, the player is the spaceship and goes throught space to save the universe. His main goal is to destroy the alien that may come on earth. The ennemy is going to come from the top of the screen.
+
+When the player is going to reach a certain level, asteroids are going to come in the canvas with a random value. If there is a collison between the player and the asteroids, the spaceship will lose one of his life.
+
+At one point, I would like to add another level which would be the last one. There would be a bigger alien that would be more diffuclt to destroy. It would be kind of a battle between the spaceship and the alien. He would be able to destroy the player. The player would win if he destroy the alien first. 
 
 ******************/
 
