a676602eaa500609f4e7461776ee9e36e02b5500
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2018-11-13 13:56:41 -0500

    Ex7: Preloading my assests

--- exercise7/js/script.js
+++ exercise7/js/script.js
@@ -7,7 +7,7 @@ For this prototype, the player is the spaceship and goes throught space to save 
 
 When the player is going to reach a certain level, asteroids are going to come in the canvas with a random value. If there is a collison between the player and the asteroids, the spaceship will lose one of his life.
 
-At one point, I would like to add another level which would be the last one. There would be a bigger alien that would be more diffuclt to destroy. It would be kind of a battle between the spaceship and the alien. He would be able to destroy the player. The player would win if he destroy the alien first. 
+At one point, I would like to add another level which would be the last one. There would be a bigger alien that would be more diffuclt to destroy. It would be kind of a battle between the spaceship and the alien. He would be able to destroy the player. The player would win if he destroy the alien first.
 
 ******************/
 
@@ -16,7 +16,9 @@ At one point, I would like to add another level which would be the last one. The
 // Description of preload
 
 function preload() {
-
+  myFont = loadFont("assets/fonts/VT323-Regular.ttf");
+  playerImage = loadImage("assets/images/player.png");
+  bulletImage = loadImage("assets/images/bullet.png");
 }
 
 
