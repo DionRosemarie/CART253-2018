3f39f602da0a685ea963b25be8606b09b9dd2532
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2018-11-20 14:17:36 -0500

    Ex8: Adding the new class
    I used the same as the last exercise because they are similar. I am going to add new variables and attributs to them.

---
+++ exercise8/js/Enemy.js
@@ -0,0 +1,48 @@
+// ENEMY FILE
+// Everything that involve the enemy is in that file
+
+
+// Variables for the enemy
+function Enemy(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.vy = 0;
+  this.vx = 0;
+  this.size = 25;
+  this.speed = speed;
+  this.image = enemyImage;
+  this.alive = true;
+}
+
+// Update of the enemy
+Enemy.prototype.update = function() {
+  this.vy = this.speed;
+  this.y = this.y + this.vy;
+  // Displaying the enemy from the top of the screen
+  if (this.y > height) {
+    this.y = 0;
+    this.x = random(0,width);
+  }
+}
+
+Enemy.prototype.handleCollision = function(bullets) {
+  // This is the information about the bullets
+  for (var i = 0; i < bullets.length; i++) {
+    var bullet = bullets[i];
+  // See if the bullet and the enemy are at the same place in the screen
+    if (bullet.x - bullet.size / 2 < this.x + this.size / 2 && bullet.x + bullet.size / 2 > this.x - this.size / 2) {
+      if (bullet.y - bullet.size / 2 < this.y + this.size / 2 && bullet.y + bullet.size / 2 > this.y - this.size / 2) {
+  // If they are at the same place, the enemy disapear
+        this.alive = false;
+      }
+    }
+  }
+}
+
+// Displaying the enemy
+Enemy.prototype.display = function() {
+  if (this.alive === false) {
+    return;
+  }
+  image(this.image, this.x, this.y, this.size, this.size);
+}
