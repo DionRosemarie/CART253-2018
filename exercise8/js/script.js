832f8ef5d4ecd76908ecde1bcf756fe306c47959
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2018-11-20 14:14:56 -0500

    Ex8: Adding the variables and creating the canvas

--- exercise8/js/script.js
+++ exercise8/js/script.js
@@ -3,15 +3,33 @@
 Outer space - Project 3 prototype
 Rose-Marie Dion
 
-This is the prototype for the second part of the game. The first part is going to allow the player to win life so when he arrives to the second part, he have more chance to kill the big alien. This particular alien is going to move around and be bigger. 
+This is the prototype for the second part of the game. The first part is going to allow the player to win life so when he arrives to the second part, he have more chance to kill the big alien. This particular alien is going to move around and be bigger.
 ******************/
+
+// variables
+//
+// This is all the variables I need for the game to work
+
+// displaying the images
+
+var playerImage;
+var bulletImage;
+var ennemyImage;
+
+// variable for the player
+var player;
+// variable for the ennemy
+var enemy;
+
 
 // preload()
 //
 // Description of preload
 
 function preload() {
-
+  playerImage = loadImage("assets/images/player.png");
+  bulletImage = loadImage("assets/images/bullet.png");
+  enemyImage = loadImage("assets/images/Enemy.png");
 }
 
 
@@ -20,6 +38,16 @@ function preload() {
 // Description of setup
 
 function setup() {
+  // Creating the canvas
+  createCanvas(800,800);
+  background(0);
+
+  // information for the Player.js
+  player = new Player(4*width/4,400,0,0,50,5,DOWN_ARROW, UP_ARROW, LEFT_ARROW, RIGHT_ARROW,90);
+
+  // information for the Ennemy.js
+  enemy = new Enemy(random(0,width),-15,5);
+
 
 }
 
