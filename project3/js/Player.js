a07e579aace1dec5c8f37d0c2ab91a32bc3a87d1
Rose-Marie Dion <dion.rosemarie@hotmail.com>
2018-12-03 14:52:24 -0500

    P3: Adding the informations to each function of the player's class

--- project3/js/Player.js
+++ project3/js/Player.js
@@ -26,10 +26,51 @@ function Player(x, y, h, w, size, speed, downKey, upKey, leftKey, rightKey, shoo
 }
 
   // Update of the player
-Player.prototype.update = function() {}
+Player.prototype.update = function() {
+  // This part allows the player to move from the top to the bottom of the canvas
+this.y += this.vy;
+this.y = constrain(this.y, 0, height - this.h);
+// This part allows the player to move from the left to the right of the canvas
+this.x += this.vx;
+this.x = constrain(this.x, 0, width - this.w);
+// Calcute the velocity based on speed and trig
+var vy = this.speed * cos(this.angle);
+var vx = this.speed * sin(this.angle);
+}
 // Displaying the player
-Player.prototype.display = function() {}
+Player.prototype.display = function() {
+  // Displaying the player witht the image
+  fill(255);
+  image(this.playerImage, this.x, this.y, this.size, this.size);
+
+}
 // handleInput of the player
-Player.prototype.handleInput = function() {}
+Player.prototype.handleInput = function() {
+  // Let the player go up
+  if (keyIsDown(this.upKey)) {
+    this.vy = -this.speed;
+  }
+
+  // Let the player go down
+  else if (keyIsDown(this.downKey)) {
+    this.vy = this.speed;
+  }
+
+  // Let the player go left
+  else if (keyIsDown(this.leftKey)) {
+    this.vx = -this.speed;
+  }
+
+  // Let the player go right
+  else if (keyIsDown(this.rightKey)) {
+    this.vx = this.speed;
+  }
+
+  // If the player doesn't press any key, the player doesn't move
+  else {
+    this.vy = 0;
+    this.vx = 0;
+  }
+}
 // handleCollision of the player
 Player.prototype.handleCollision = function() {}
